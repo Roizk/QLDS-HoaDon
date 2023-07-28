4b62dc29377f36f278c1b52939f7a990b425cf56
Roizk <hunglmhtvtbr@gmail.com>
2023-07-28 18:48:51 +0700

    impl

--- QLHoaDon/src/Persistence/HoaDonDAO.java
+++ QLHoaDon/src/Persistence/HoaDonDAO.java
@@ -1,8 +1,11 @@
 package QLHoaDon.src.Persistence;
+import java.util.List;
 
+import Domain.*;
 public interface HoaDonDAO {
     public void add(HoaDon hoadon);
     public void update(HoaDon hoadon);
+    public void delete(int ma);
     HoaDon getHoaDonByma(int ma); 
     List<HoaDon> getAllsHoaDon();
 }
