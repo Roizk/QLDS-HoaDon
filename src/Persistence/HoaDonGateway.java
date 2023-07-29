8893d31137454db515dfc4122bf0d99f1e076bd2
thenghia2502 <nghiapham250203@gmail.com>
2023-07-29 16:30:25 +0700

    add class HoaDonJdbcGateway and fix HoaDonGateway

--- src/Persistence/HoaDonGateway.java
+++ src/Persistence/HoaDonGateway.java
@@ -3,11 +3,17 @@ package Persistence;
 import java.util.List;
 
 import Domain.Model.HoaDonTienDien;
+import Domain.Model.HoaDonTienDienNN;
+import Domain.Model.HoaDonTienDienVN;
 
 public interface HoaDonGateway {
-    public void add(HoaDonTienDien hoadon);
-    public void update(HoaDonTienDien hoadon);
+	public void addHoaDonTienDienNN(HoaDonTienDienNN hoadon);
+    public void addHoaDonTienDienVN(HoaDonTienDienVN hoadon);
+    public void updateHoaDonTienDienNN(HoaDonTienDienNN hoadon);
+    public void updateHoaDonTienDienVN(HoaDonTienDienVN hoadon);
     public void delete(int ma);   
-    HoaDonTienDien getHoaDonByMa(int ma);
-    List<HoaDonTienDien> getAllsHoaDon();
+    HoaDonTienDienNN getHoaDonNNByMa(int ma);
+    HoaDonTienDienVN getHoaDonVNByMa(int ma);
+    List<HoaDonTienDienNN> getAllsHoaDonNN();
+    List<HoaDonTienDienVN> getAllsHoaDonVN();
 }
