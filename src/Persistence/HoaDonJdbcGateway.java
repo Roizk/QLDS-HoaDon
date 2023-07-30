561d84509c3862d577f0566ba579d42017742d75
TTPhuc <tthanhphuc753@gmail.com>
2023-07-30 17:53:37 +0700

    fix connection string

--- src/Persistence/HoaDonJdbcGateway.java
+++ src/Persistence/HoaDonJdbcGateway.java
@@ -23,7 +23,7 @@ public class HoaDonJdbcGateway implements HoaDonGateway{
 	private Connection connection;
 	
 	public HoaDonJdbcGateway() {
-		String dbURL="jdbc:sqlserver://localhost:1433;databaseName=quanlyhoadon,integratedSecurity=true";
+		String dbURL="jdbc:sqlserver://localhost:1433;databaseName=quanlyhoadon;integratedSecurity=true";
 		
 		try {
 			connection = DriverManager.getConnection(dbURL);
