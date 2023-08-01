9eb5db9e8c931a39375526ae42cc428228dfa7ec
QuocAnhBiKu <anhbr2016@gmail.com>
2023-08-01 14:11:15 +0700

    sua lai controller va view

--- src/Presentation/HoaDonTienDienController.java
+++ src/Presentation/HoaDonTienDienController.java
@@ -1,14 +1,10 @@
 package Presentation;
 
-import java.util.List;
 
 import Domain.Command.Command;
-import Domain.Model.HoaDonTienDienNN;
-import Domain.Model.HoaDonTienDienVN;
-import Persistence.HoaDonJdbcGateway;
 
 public class HoaDonTienDienController {
-    private HoaDonJdbcGateway hoaDonJdbcGateway = new HoaDonJdbcGateway();
+    
     private HoaDonTienDienView hoaDonTienDienView;
 
     public HoaDonTienDienController() {
@@ -20,13 +16,6 @@ public class HoaDonTienDienController {
 
     public void execute(Command command) {
         command.execute();
-    }
-    public List<HoaDonTienDienVN> getAllsHoaDonVN() {
-        return hoaDonJdbcGateway.getAllsHoaDonVN();
-    }
-
-    public List<HoaDonTienDienNN> getAllsHoaDonNN() {
-        return hoaDonJdbcGateway.getAllsHoaDonNN();
     }
 
 }
